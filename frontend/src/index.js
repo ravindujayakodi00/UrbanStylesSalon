b9f043feba5d2698c9a0d24f04db623afc9d35cc
Ravindu Jayakodi <109682931+ravindujayakodi00@users.noreply.github.com>
2022-12-19 17:43:45 +0530

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import { ProductsContextProvider } from './context/productContext';
+import './styles/products.css';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <AuthContextProvider>
+    <ProductsContextProvider>
+
+       <App />
+    </ProductsContextProvider>
+    </AuthContextProvider>
+
   </React.StrictMode>
 );
-
 
